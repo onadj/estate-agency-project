8877018ab1b1edbd30a6d18f9d32af9aba88a8dc
onadj <oliver.nad@gmail.com>
2020-12-05 21:37:54 +0000

    initial commit, created project,created app property with models, views, urls, templates, media file, everything migrated tested and its working

---
+++ property/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Property, Category
+
+
+admin.site.register(Property)
+admin.site.register(Category)
---
+++ property/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Property, Category
+
+# Create your views here.
+def property_list(request):
+    property_list = Property.objects.all()
+    template = 'property/list.html'
+    context = {
+              'property_list' : property_list
+    }
+
+    return render(request ,template, context)
+
+def property_detail(request, id):
+    property_detail = Property.objects.get(id=id)
+    template = 'property/detail.html'
+    context = {
+              'property_detail' : property_detail
+    }
+    return render(request, template, context)
