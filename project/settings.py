f6cb580ce460b94fbb1ec4b284778909dfcdbc42
onadj <oliver.nad@gmail.com>
2020-12-07 17:49:47 +0000

    deployment part 2 - ALLOWED_HOSTS changed, config:set  DISABLE_COLLECTSTATIC=1, procfile,...

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'j6kj@*_wj*fovfd75)hz46s!ad^ewcg1h7=j6i9qtl38a-+2zp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['estate-agency-project.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -80,11 +80,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-  }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
+# Password validation
+
 
 
 # Password validation
